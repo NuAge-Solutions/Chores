ba2811e7a03869fefce82362f2dc19b65800ad77
Tony Osibov <tony.osibov@gmail.com>
2015-11-23 17:31:44 -0800

    fixed which lookup logic

--- Code/ChoreTask.swift
+++ Code/ChoreTask.swift
@@ -6,49 +6,54 @@ private func string_trim(string: NSString!) -> String {
     return string.stringByTrimmingCharactersInSet(.whitespaceAndNewlineCharacterSet()) ?? ""
 }
 
-private func chore_task(command: String, _ arguments: [String] = [String](), stdin: String = "", dir: String? = nil) -> ChoreResult {
+internal func chore_task(command: String, _ arguments: [String] = [String](), stdin: String = "", dir: String? = nil) -> ChoreResult {
     let task = NSTask()
-
+    
     task.launchPath = command
     task.arguments = arguments
     
     if let d = dir {
         task.currentDirectoryPath = d
     }
-
-    if !(task.launchPath! as NSString).absolutePath {
-        task.launchPath = (chore_task("/usr/bin/which", [task.launchPath!])).stdout
+    
+    if !(command as NSString).absolutePath {
+        task.launchPath = (chore_task("/bin/bash", ["-l", "-c", "which " + command])).stdout
     }
-
-    if !NSFileManager.defaultManager().fileExistsAtPath(task.launchPath!) {
-        return (255, "", String(format: "%@: launch path not accessible", task.launchPath!))
+    
+    if let lp = task.launchPath {
+        if !NSFileManager.defaultManager().fileExistsAtPath(lp) {
+            return (255, "", "\(lp): launch path not accessible")
+        }
     }
-
+    
     if stdin.characters.count > 0 {
         let stdinPipe = NSPipe()
         task.standardInput = stdinPipe
+        
         let stdinHandle = stdinPipe.fileHandleForWriting
-
+        
         if let data = stdin.dataUsingEncoding(NSUTF8StringEncoding) {
             stdinHandle.writeData(data)
             stdinHandle.closeFile()
         }
     }
-
+    
     let stderrPipe = NSPipe()
     task.standardError = stderrPipe
+    
     let stderrHandle = stderrPipe.fileHandleForReading
-
+    
     let stdoutPipe = NSPipe()
     task.standardOutput = stdoutPipe
+    
     let stdoutHandle = stdoutPipe.fileHandleForReading
-
+    
     task.launch()
     task.waitUntilExit()
-
+    
     let stderr = string_trim(NSString(data: stderrHandle.readDataToEndOfFile(), encoding: NSUTF8StringEncoding)) ?? ""
     let stdout = string_trim(NSString(data: stdoutHandle.readDataToEndOfFile(), encoding: NSUTF8StringEncoding)) ?? ""
-
+    
     return (task.terminationStatus, stdout, stderr)
 }
 
@@ -60,32 +65,32 @@ public prefix func > (command: String) -> ChoreResult {
 
 public prefix func > (command: [String]) -> ChoreResult {
     switch command.count {
-        case 0:
-            return (0, "", "")
-        case 1:
-            return chore_task(command[0])
-        default:
-            break
+    case 0:
+        return (0, "", "")
+    case 1:
+        return chore_task(command[0])
+    default:
+        break
     }
-
+    
     return chore_task(command[0], Array(command[1..<command.count]))
 }
 
 public prefix func > (command: (String, String)) -> ChoreResult {
-    return chore_task(command[1], dir: command[0])
+    return chore_task(command.1, dir: command.0)
 }
 
 public prefix func > (command: (String, [String])) -> ChoreResult {
-    switch command.count {
-        case 0:
-            return (0, "", "")
-        case 1:
-            return chore_task(command[1], dir: command[0])
-        default:
-            break
+    switch command.1.count {
+    case 0:
+        return (0, "", "")
+    case 1:
+        return chore_task(command.1[0], dir: command.0)
+    default:
+        break
     }
     
-    return chore_task(command[1][0], Array(command[1][1..<command[1].count]), dir: command[0])
+    return chore_task(command.1[0], Array(command.1[1..<command.1.count]), dir: command.0)
 }
 
 infix operator | {}
@@ -98,7 +103,7 @@ public func | (left: ChoreResult, right: [String]) -> ChoreResult {
     if left.result != 0 {
         return left
     }
-
+    
     let arguments = right.count >= 2 ? Array(right[1..<right.count]) : [String]()
     return chore_task(right[0], arguments, stdin: left.stdout)
 }
@@ -107,7 +112,7 @@ public func | (left: ChoreResult, right: ((String) -> String)) -> ChoreResult {
     if left.result != 0 {
         return left
     }
-
+    
     return (0, right(left.stdout), "")
 }
 
